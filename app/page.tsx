f63e88c8d0d28ef8cf0d6f51f027c09663121a6a
OliWebDevO <oliver-vdb@hotmail.com>
2025-06-24 19:36:15 +0200

    Preloader test

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 // import Image from "next/image";
-
+'use client';
 import Slider from "@/components/models/MaterialUiSlider/Slider";
 import NavBar from "@/components/NavBar";
 import Contact from "@/sections/Contact";
@@ -10,12 +10,23 @@ import Hero from "@/sections/Hero";
 import LogoSection from "@/sections/LogoSection";
 import ShowcaseSection from "@/sections/ShowcaseSection";
 import TechStack from "@/sections/TechStack";
+import Preloader from "@/components/Lottie/LottiePreloader";
+import { useEffect, useState } from "react";
 // import Testimonials from "@/sections/Testimonials";
 
 export default function Home() {
+
+   const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Your asset loading logic here
+    setTimeout(() => setLoading(false), 2000); // Example: 2s delay
+  }, []);
   return (
+
     <>
-      <main>
+     {loading && <Preloader />}
+      <main style={{ opacity: loading ? 0 : 1, transition: "opacity 0.5s" }}>
         <NavBar/>
         <Hero/>
         <Slider/>
