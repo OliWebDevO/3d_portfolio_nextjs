0fdea35be972153448fc71901f86ae37db46401f
OliWebDevO <oliver-vdb@hotmail.com>
2026-02-17 16:57:56 +0100

    scroll

--- components/ScrollToTop.tsx
+++ components/ScrollToTop.tsx
@@ -21,7 +21,6 @@ export default function ScrollToTop() {
   return (
     <button
       onClick={handleClick}
-      onTouchStart={handleClick}
       aria-label="Scroll to top"
       className={`fixed bottom-6 right-6 z-50 size-12 rounded-full bg-white/90 shadow-lg flex items-center justify-center transition-all duration-300 ${
         visible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-4 pointer-events-none'
